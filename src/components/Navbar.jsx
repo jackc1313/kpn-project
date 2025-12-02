16817ed927590db063a9e7261725cfd8f815daed
Jacopo <179255721+jackc1313@users.noreply.github.com>
2025-12-02 11:22:40 +0100

    refactor navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import "../styles/global.css";
 
 export default function Navbar(){
     return(
-        <nav className="m-5 flex justify-center items-center lg:p-5 relative box-shadow-bottom">
+        <nav className="m-5 flex justify-between gap-10 items-center lg:p-5 lg:pr-40 relative box-shadow-bottom">
             <img src="src/assets/logo.png" alt="KPN logo" className="logo-navbar"></img>
-            <ul className="nav-voices-font flex gap-10 text-xl text-lime-400 font-semibold italic uppercase mx-auto">
+            <ul className="nav-voices-font flex gap-10 text-xl text-lime-400 font-semibold italic uppercase">
                 <li>Home</li>
                 <li>il park</li>
                 <li>la sala prove</li>
